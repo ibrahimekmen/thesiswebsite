4cf8666b6bf8866891528fcc372ef35b11a865ba
İbrahim Ekmen <ibrahimekmentr70@gmail.com>
2022-05-21 23:57:56 +0300

    general improvements

--- services.js
+++ services.js
@@ -9,7 +9,7 @@ function getTopTrends(lang, time) {
     return MongoClient.connect(database_URI)
         .then(mc => mc.db('data_analysis') 
                     .collection('trends')
-                    .find({"created_at" : {$exists: true, $gt: time}, "lang" : lang})
+                    .find({"as_of" : {$exists: true, $type: "date", $gt: time}, "lang" : lang})
                     .limit(10)
                     .sort({"tweet_volume": -1})
                     .toArray()
@@ -17,11 +17,11 @@ function getTopTrends(lang, time) {
         .catch(e => console.log(e));
 }
 
-function getTodayTopTrends(lang) {
+function getTodayTopTrends(lang, time) {
     return MongoClient.connect(database_URI)
         .then(mc => mc.db('data_analysis') 
                     .collection('trends')
-                    .find({"created_at" : {$exists: true}, "lang" : lang})
+                    .find({"created_at" : {$exists: true, $type: "date", $gt: time}, "lang" : lang})
                     .limit(10)
                     .sort({"tweet_volume": -1})
                     .toArray()
@@ -44,9 +44,36 @@ function getFavoriteTweets(lang, time) {
     return MongoClient.connect(database_URI)
         .then(mc => mc.db('data_analysis')
                     .collection('tweets')
-                    .find({"created_at" : {$exists: true, $type: "date", $gt: time}, "lang" : lang})
+                    .find({"created_at" : {$gt: time}, "lang" : lang})
                     .limit(10)
-                    .sort({"favorite_count": -1, "created_at": -1,})
+                    .sort({"favorite_count": -1})
+                    .toArray()
+                    .then(as => (mc.close(), as)))
+        .catch(e => console.log(e));
+}
+
+function getFavoriteTweets2(lang, time) {
+    const pipeline = [
+        {$sort: {"favorite_count": -1}},
+        {$match: 
+            {
+                "created_at": {$exists: true, $type: "date", $gt: time}, 
+                "lang": lang
+            }
+        },
+        {$group: {
+            "_id": "$id",
+            "favorite_count": {$max: "$favorite_count"},
+            "trend_name": {$max: "$trend_name"},
+            "full_text": {$max: "$full_text"},
+        }},
+        {$limit: 10}
+        
+    ]
+    return MongoClient.connect(database_URI)
+        .then(mc => mc.db('data_analysis')
+                    .collection('tweets')
+                    .aggregate(pipeline)
                     .toArray()
                     .then(as => (mc.close(), as)))
         .catch(e => console.log(e));
@@ -77,11 +104,25 @@ function getMostPositiveTweet(trendName){
         .catch(e => console.log(e));
 }
 
+function getMostNegativeTweet(trendName){
+    return MongoClient.connect(database_URI)
+        .then(mc => mc.db('data_analysis') 
+                    .collection('tweets')        
+                    .find({trend_name: trendName})
+                    .limit(1)
+                    .sort({vader_result: 1})
+                    .toArray()
+                    .then(as => (mc.close(), as)))
+        .catch(e => console.log(e));
+}
+
 module.exports = {
     getTrendByName,
     getTodayTopTrends,
     getMostPositiveTweet,
+    getMostNegativeTweet,
     getTopTrends,
     getFavoriteTweets,
+    getFavoriteTweets2,
     getFavoriteTweetsToday
 }
