9ae1bbb4f5773c5cc92cb834bbd54d6fd98795b8
İbrahim Ekmen <ibrahimekmentr70@gmail.com>
2022-05-22 23:18:39 +0300

    routes fixed

--- controllers/trendPageController.js
+++ controllers/trendPageController.js
@@ -3,6 +3,9 @@ const services = require('../services.js');
 function render(req,res){
     trendName = req.params.trendName;
     return Promise.all([services.getTrendByName(trendName),services.getMostPositiveTweet(trendName),services.getMostNegativeTweet(trendName)]).then(data =>{
+        if (data[0][0] == null){
+            res.redirect('/error')
+        }
         res.render('trend',{
             trend: data[0][0],
             mostPos: data[1][0]['full_text'],
